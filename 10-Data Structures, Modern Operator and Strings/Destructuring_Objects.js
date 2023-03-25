936bea70d42c36114e4594a331c3de97d226bbf3
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2023-03-25 22:01:10 +0530

    object

---
+++ 10-Data Structures, Modern Operator and Strings/Destructuring_Objects.js
@@ -0,0 +1,69 @@
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function (obj) {
+        console.log(obj);
+    },
+};
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'via del sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+
+
+const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+
+let a = 111;
+let b = 999;
+const obj =
+{
+    a: 23,
+    b: 7,
+    c: 14
+};
+console.log(a, b);
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested Objects
+
+const { fri: { open: o, close: c } } = hours;
+console.log(o, c);
+
